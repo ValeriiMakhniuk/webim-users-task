dc9f8c06a436546cfb3b3d52cb998a7e1b87847e
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-08-07 19:15:03 +0300

    add api call functions

---
+++ src/api/api.ts
@@ -0,0 +1,90 @@
+import axios from 'axios';
+
+import { Routes } from './routes';
+
+interface MAuthToken {
+  username: string;
+  password: string;
+}
+
+interface MUser {
+  id?: number;
+  username: string;
+  first_name?: string;
+  last_name?: string;
+  password: string;
+  is_active: boolean;
+  last_login?: string;
+  is_superuser?: boolean;
+}
+
+export const getAuthToken = async (
+  credentials: MAuthToken
+): Promise<string> => {
+  return await axios
+    .post(Routes.AuthToken, credentials)
+    .then((res) => res.data);
+};
+
+export const getUsers = async (token: string): Promise<MUser[]> => {
+  return await axios
+    .get(Routes.Users, {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    })
+    .then((res) => res.data);
+};
+
+export const getUser = async (
+  userId: string,
+  token: string
+): Promise<MUser> => {
+  return await axios
+    .get(`${Routes.Users}/${userId}/`, {
+      headers: {
+        Authorization: `Token ${token}`,
+      },
+    })
+    .then((res) => res.data);
+};
+
+export const postUser = async (userData: MUser, token: string) => {
+  await axios.post(Routes.Users, userData, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
+};
+
+export const putUser = async (
+  userId: string,
+  userData: MUser,
+  token: string
+) => {
+  await axios.put(`${Routes.Users}/${userId}/`, userData, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
+};
+
+export const patchUser = async (
+  userId: string,
+  userData: MUser,
+  token: string
+) => {
+  await axios.patch(`${Routes.Users}/${userId}/`, userData, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
+};
+
+export const deleteUser = async (userId: string, token: string) => {
+  await axios.delete(`${Routes.Users}/${userId}/`, {
+    headers: {
+      Authorization: `Token ${token}`,
+    },
+  });
+};
