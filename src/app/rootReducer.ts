4ee1c9ea40f922fdeab3a66bb87ec7d62051be65
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-08-12 12:24:30 +0300

    apply usersReducer

--- src/app/rootReducer.ts
+++ src/app/rootReducer.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from '@reduxjs/toolkit';
 
 import { authReducer } from '../feauteres/auth/authSlice';
+import { usersReducer } from '../feauteres/users/usersSlice';
 
 export const rootReducer = combineReducers({
   auth: authReducer,
+  users: usersReducer,
 });
 
 export type RootState = ReturnType<typeof rootReducer>;
