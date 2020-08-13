74b6c86ca38818f9c8a6207ab1487515f15663f6
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-08-13 23:04:58 +0300

    Updates

---
+++ precache-manifest.66bad9b3ede2347d1507ff5bce6ccbde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22de5a401afb39cabf5ad6a319c67ca6",
+    "url": "/webim-users-task/index.html"
+  },
+  {
+    "revision": "79fe4ccd8e45500c31f6",
+    "url": "/webim-users-task/static/css/2.3aa9a6e6.chunk.css"
+  },
+  {
+    "revision": "967acbb7dea233adeea5",
+    "url": "/webim-users-task/static/css/main.4d47be83.chunk.css"
+  },
+  {
+    "revision": "79fe4ccd8e45500c31f6",
+    "url": "/webim-users-task/static/js/2.8c09dc5f.chunk.js"
+  },
+  {
+    "revision": "27f1c73f1dc921bc79aa41830d4cd146",
+    "url": "/webim-users-task/static/js/2.8c09dc5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "967acbb7dea233adeea5",
+    "url": "/webim-users-task/static/js/main.33f1e293.chunk.js"
+  },
+  {
+    "revision": "627e90e86d92d81f3508",
+    "url": "/webim-users-task/static/js/runtime-main.d436b5ac.js"
+  }
+]);
