415479e5fcd2fb48468e8eeeafadec94a9923500
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-08-27 11:47:46 +0300

    add top navbar with logout button

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,22 +2,38 @@ import React from 'react';
 
 import { Route, Redirect } from 'react-router-dom';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../app/rootReducer';
+import { deauthenticate } from '../feauteres/auth/authSlice';
 
 import { AuthForm } from '../feauteres/auth/AuthForm';
 import { UsersPage } from '../feauteres/users/usersPage';
-import { Container } from 'react-bootstrap';
+import { Container, Navbar, Button } from 'react-bootstrap';
 
 export const App: React.FC = () => {
+  const dispatch = useDispatch();
   const { isAuthenticated } = useSelector((state: RootState) => state.auth);
 
+  const handleLogOut = () => {
+    dispatch(deauthenticate());
+  };
+
   return (
-    <Container fluid={true} as='main' className='d-flex flex-column h-100'>
-      <Route exact={true} path='/'>
-        {!isAuthenticated ? <Redirect to='/login' /> : <UsersPage />}
-      </Route>
-      <Route exact={true} path='/login' component={AuthForm} />
-    </Container>
+    <>
+      {isAuthenticated && (
+        <Navbar bg='light' expand='lg'>
+          <Navbar.Brand>Users CMS</Navbar.Brand>
+          <Button className='ml-auto' onClick={handleLogOut}>
+            Log out
+          </Button>
+        </Navbar>
+      )}
+      <Container fluid={true} as='main' className='d-flex flex-column h-100'>
+        <Route exact={true} path='/'>
+          {!isAuthenticated ? <Redirect to='/login' /> : <UsersPage />}
+        </Route>
+        <Route exact={true} path='/login' component={AuthForm} />
+      </Container>
+    </>
   );
 };
