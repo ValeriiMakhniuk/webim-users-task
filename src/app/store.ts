f24d55056975c5f73f679c042640694db2e0f2b5
Valerii Makhniuk <vmakhnyuk@yandex.ru>
2020-08-27 11:47:23 +0300

    add get token from localStorage functionality

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,20 @@
 import { configureStore, Action } from '@reduxjs/toolkit';
 import { ThunkAction } from 'redux-thunk';
 
+import localStore from 'store';
+
 import { rootReducer, RootState } from './rootReducer';
+
+const token = localStore.get('token');
 
 export const store = configureStore({
   reducer: rootReducer,
+  preloadedState: {
+    auth: {
+      isAuthenticated: !!token,
+      token,
+    },
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
